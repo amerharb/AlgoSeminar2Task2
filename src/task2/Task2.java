565429ffd6ddb70c6cc560ae054b7a144787f89a
amerharb <amer.harb@gmail.com>
2015-12-11 12:20:02 +0100

    init input M value and N value

---
+++ src/task2/Task2.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task2;
+
+/**
+ *
+ * @author Amer
+ */
+public class Task2
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        while (true) {
+            System.out.println("");
+            System.out.println("--------------------------------------------------------------------------------------");
+            System.out.println("");
+
+            java.util.Scanner sc = new java.util.Scanner(System.in);
+            String ans;
+
+            System.out.println("Enter the Value of M");
+            System.out.print("M=");
+            ans = sc.nextLine();
+            int M = Integer.parseInt(ans);
+
+            System.out.println("Enter the Value of N");
+            System.out.print("N=");
+            ans = sc.nextLine();
+            int N = Integer.parseInt(ans);
+
+            System.out.println(M);
+            System.out.println(N);
+        }
+    }
+
+}
